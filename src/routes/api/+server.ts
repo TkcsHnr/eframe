666f90d69356310e75045fcb616abbc3ddd0a3e0
TkcsHnr <tkcshnr@gmail.com>
2024-09-27 00:07:38 +0200

    no orientation field in response

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,16 +1,11 @@
 import { floydSteinbergDither } from '$lib/jimp';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({url}) => {
+export const GET: RequestHandler = async ({ url }) => {
 	const { pixelPairArray } = await floydSteinbergDither(
 		'https://upload.wikimedia.org/wikipedia/commons/f/f2/Comic_mural_Le_jeune_Albert%2C_Yves_Chaland%2C_Bruxelles.jpg',
 		false
 	);
 
-	return new Response(
-		JSON.stringify({
-			pixelPairArray,
-			portrait: false
-		})
-	);
+	return new Response(JSON.stringify(pixelPairArray));
 };
