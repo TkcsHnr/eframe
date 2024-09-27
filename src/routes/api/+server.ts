984957c5a0b46789071c359786b36daad0c270d7
TkcsHnr <tkcshnr@gmail.com>
2024-09-27 18:30:32 +0200

    random image api

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,11 +1,8 @@
 import { floydSteinbergDither } from '$lib/jimp';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ url }) => {
-	const { pixelPairArray } = await floydSteinbergDither(
-		'https://upload.wikimedia.org/wikipedia/commons/f/f2/Comic_mural_Le_jeune_Albert%2C_Yves_Chaland%2C_Bruxelles.jpg',
-		false
-	);
+export const GET: RequestHandler = async () => {
+	const { pixelPairArray } = await floydSteinbergDither("https://picsum.photos/1600/1200", false);
 
 	return new Response(JSON.stringify(pixelPairArray));
 };
