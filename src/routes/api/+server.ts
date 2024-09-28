4e3ae24cf89a056a772515580c1e91d7c6151afa
TkcsHnr <tkcshnr@gmail.com>
2024-09-28 14:48:34 +0200

    supabase current image demo

--- src/routes/api/+server.ts
+++
@@ -1,8 +0,0 @@
-import { floydSteinbergDither } from '$lib/jimp';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	const { pixelPairArray } = await floydSteinbergDither("https://picsum.photos/1600/1200", false);
-
-	return new Response(JSON.stringify(pixelPairArray));
-};
